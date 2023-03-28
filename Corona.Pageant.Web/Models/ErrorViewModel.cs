d02b478f5309a7f319fa65eb5543dcb23826773c
Glen Souza <glenster75@gmail.com>
2023-03-28 15:25:01 -0700

    initial commit

---
+++ Corona.Pageant.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Corona.Pageant.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
